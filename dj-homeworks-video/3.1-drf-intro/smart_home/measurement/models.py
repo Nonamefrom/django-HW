8a97a3d66541bd5b0eec060672f5056299795ff1
DenisIlin <denni-dog@mail.ru>
2024-09-27 22:20:57 +0300

    Make homework SmartHome with temperature

--- dj-homeworks-video/3.1-drf-intro/smart_home/measurement/models.py
+++ dj-homeworks-video/3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=250, blank=True, null=True)
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, related_name='measurements', on_delete=models.CASCADE)
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
